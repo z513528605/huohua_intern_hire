adb7a0ca9e0bfef3e6dd1e34c00da388dbafa4ad
Z.CHENHONG <53003065+z513528605@users.noreply.github.com>
2020-06-22 10:56:23 +0800

    Update assignment.py
    Applicants

--- assignment.py
+++ assignment.py
@@ -1,5 +1,5 @@
 import json
-import pandas as pd
+import csv
 from adapter import StreamAdapter
 from typing import Dict, Hashable, Iterable
 
@@ -10,56 +10,41 @@ class CourseWareB(StreamAdapter):
     答案状态在commonComponentState下的4cb5f12f9e164c6c545a55202bc818f2下的answer字段
     正确答案是1，2，0，36V
     """
-    @staticmethod
-    def __extract_panel_status(panel_status: Dict[str, str]) -> tuple:
-        panel_state = [0, 0, 0, 0]
-        for idx, n in panel_status.items():
-            panel_state[int(idx)] = int(n)
-        return tuple(panel_state)
+    # @staticmethod
+    # def __extract_panel_status(panel_status: Dict[str, str]) -> tuple:
+    #     panel_state = [0, 0, 0, 0]
+    #     for idx, n in panel_status.items():
+    #         panel_state[int(idx)] = int(n)
+    #     return tuple(panel_state)
 
     @classmethod
     def load_raw_state(cls, raw_state: str) -> Hashable:
         state = json.loads(raw_state).get("commonComponentState")
-        panel_status = (0, 0, 0, 0)
+        panel_status = [0, 0, 0, 0]
         if state is not None:
 
             if "4cb5f12f9e164c6c545a55202bc818f2" in state:
-                panel_status = cls.__extract_panel_status(
-                    state["4cb5f12f9e164c6c545a55202bc818f2"]["answer"]
-                )
+                panel_status = state["4cb5f12f9e164c6c545a55202bc818f2"]["answer"]
 
         return panel_status
 
     @classmethod
     def is_user_right(cls, stream: Iterable) -> bool:
-        right_ans = (1, 2, 0, 3)  # 我这里没有理解‘36V’的含义，猜想了一下
-        return list(stream)[-1] == right_ans
+        right_ans = [1, 2, 0, 3]  # 我这里没有理解‘36V’的含义，猜想了一下
+        return list(stream) == right_ans
 
 
 if __name__ == "__main__":
     """
     在这里处理日志输出，输出结果为result.csv，三个字段为：学生ID，状态，是否为正确状态
     """
-    # 使用CourseWareB处理data.csv时，中间出现了小问题，不知道是不是我的csv读取方式不匹配
-    # 于是我换了一种方式处理
-    total_data = pd.read_csv('data.csv', header=None, converters={8: str}, delimiter="\t")
-    cstate = total_data[3]
-    # 清除第一行格式错误的数据并重新编号，如不这样做在下面循环从1开始
-    cstate.drop([0], inplace=True)
-    cstate.reset_index(drop=True, inplace=True)
-    # 变量初始化
-    right_answer = [1, 2, 0, 3]
-    answer = []
-    bool_state = [0]*len(cstate)
-    # 提取所需数据
-    for i in range(0, len(cstate)):
-        state = json.loads(cstate[i]).get("commonComponentState")
-        answer.append(state["4cb5f12f9e164c6c545a55202bc818f2"]["answer"])
-        bool_state[i] = answer[i] == right_answer
-    # 制作result.csv，把上述得到的数据存入，删除第一行是使存入数据与原始数据行数相同
-    total_data.drop(total_data.columns[1:5], axis=1, inplace=True)
-    total_data.drop([0], inplace=True)
-    total_data.insert(1, 'state', answer)
-    total_data.insert(2, 'bool_state', bool_state)
-    total_data.columns = ['学生ID', '状态', '是否为正确状态']
-    total_data.to_csv('result.csv', encoding='GB18030', header=True, index=False)  # 保存并防止汉字出现乱码
+# __extract_panel_status函数输入是字典格式，而load_raw_state函数中的"answer"字段中的数据是列表格式，故此函数在CourseWareB中应该是不必要的
+# 在example.py中的"status"字段中的数据应该是字典格式，在data.csv文件中没有找到相应字段，不太确定。
+csv_writer = csv.writer(open('result.csv', 'w', encoding='GB18030', newline=''))
+csv_writer.writerow(['学生ID', '状态', '是否为正常状态'])
+with open('data.csv', 'r') as f:
+    data = list(csv.reader(f, delimiter='\t'))
+    for i in range(1, len(data)):
+        state3 = CourseWareB.load_raw_state(data[i][3])
+        bool_state = CourseWareB.is_user_right(state3)
+        csv_writer.writerow([data[i][0], state3, bool_state])
